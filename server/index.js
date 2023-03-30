ce1afd7df0e4bad219aeb28279325ba1b1425412
Liam Lyness <liamlyness97@gmail.com>
2023-03-30 15:59:05 +0100

    Setting up first route

--- server/index.js
+++ server/index.js
@@ -7,3 +7,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`);
 });
+
+app.get('/posts', (req, res) => {
+    res.send(console.log('Posts page'))
+})
