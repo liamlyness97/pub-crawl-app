636dc6eeb08424a3c805eb24ab5436b969178e7c
Liam Lyness <liamlyness97@gmail.com>
2023-03-30 16:43:35 +0100

    Fixing issues with client folder

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static('./client'));
+
 app.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`);
 });
 
 app.get('/posts', (req, res) => {
-    res.send(console.log('Posts page'))
+    res.sendFile('./client/all-posts.html', {root: __dirname })
 })
